b70fae6732bfc75df1c00231366d61a9d118e0ed
AvinashSingh786 <u14043778@tuks.co.za>
2017-05-14 23:45:47 +0200

    graphs

--- eda.py
+++ eda.py
@@ -3,6 +3,7 @@ import numpy as np
 import sqlite3
 import matplotlib.pyplot as plt
 from matplotlib import style
+import seaborn as sns
 from Color import Color
 
 
@@ -26,96 +27,105 @@ df = pd.read_sql_query("SELECT * FROM Claims",conn,  coerce_float=True, parse_da
 
 
 ##### Get frequency counts
-print(Color.BLUE+"\n\n\nFrequency of Name\n "+Color.END)
-print(df['Name'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Name\n "+Color.END)
+# print(df['Name'].value_counts( dropna=False))
+# name = df.groupby('Name')['Name'].count().plot( kind='bar', title='Name Distribution', grid=True, legend=True, sort_columns=True)
+# ticks = name.xaxis.get_ticklocs()
+# ticklabels = [l.get_text() for l in name.xaxis.get_ticklabels()]
+# name.xaxis.set_ticks(ticks[::100])
+# name.xaxis.set_ticklabels(ticklabels[::100])
+# plt.show()
 
-print(Color.BLUE+"\n\n\nFrequency of Surname\n "+Color.END)
-print(df['Surname'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Surname\n "+Color.END)
+# print(df['Surname'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Age\n "+Color.END)
-print(df['Age'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Age\n "+Color.END)
+# print(df['Age'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Gender\n "+Color.END)
-print(df['Gender'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Gender\n "+Color.END)
+# print(df['Gender'].value_counts( dropna=False))
+# df.groupby('Gender')['Gender'].count().plot( kind='bar', title='Gender Distribution', grid=True, legend=True)
+# plt.show()
 
-print(Color.BLUE+"\n\n\nFrequency of Marital Status\n "+Color.END)
-print(df['Marital_Status'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Marital Status\n "+Color.END)
+# print(df['Marital_Status'].value_counts( dropna=False))
+# df.groupby('Marital_Status')['Marital_Status'].count().plot( kind='pie', title='Marital Status Distribution', sort_columns=True,  grid=True, legend=True)
+# plt.show()
 
-print(Color.BLUE+"\n\n\nFrequency of Date Of Birth\n "+Color.END)
-print(df['Date_Of_Birth'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Date Of Birth\n "+Color.END)
+# print(df['Date_Of_Birth'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Sum Insured\n "+Color.END)
-print(df['Sum_Insured'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Sum Insured\n "+Color.END)
+# print(df['Sum_Insured'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policies Revenue\n "+Color.END)
-print(df['Policies_Revenue'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policies Revenue\n "+Color.END)
+# print(df['Policies_Revenue'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Start Date\n "+Color.END)
-print(df['Policy_Start'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Start Date\n "+Color.END)
+# print(df['Policy_Start'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy End Date\n "+Color.END)
-print(df['Policy_End'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy End Date\n "+Color.END)
+# print(df['Policy_End'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Fraudulent Claims\n "+Color.END)
-print(df['Fraudulent_Claim'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Fraudulent Claims\n "+Color.END)
+# print(df['Fraudulent_Claim'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Fraud Reason\n "+Color.END)
-print(df['Fraudulent_Claim_Reason'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Fraud Reason\n "+Color.END)
+# print(df['Fraudulent_Claim_Reason'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Date Of Loss\n "+Color.END)
-print(df['Date_Of_Loss'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Date Of Loss\n "+Color.END)
+# print(df['Date_Of_Loss'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Date Of Claim \n "+Color.END)
-print(df['Date_Of_Claim'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Date Of Claim \n "+Color.END)
+# print(df['Date_Of_Claim'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Broker ID\n "+Color.END)
-print(df['Broker_ID'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Broker ID\n "+Color.END)
+# print(df['Broker_ID'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Insurer ID\n "+Color.END)
-print(df['Insured_ID'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Insurer ID\n "+Color.END)
+# print(df['Insured_ID'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Kind Of Loss\n "+Color.END)
-print(df['Kind_Of_Loss'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Kind Of Loss\n "+Color.END)
+# print(df['Kind_Of_Loss'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Claim Amount\n "+Color.END)
-print(df['Claim_Amount'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Claim Amount\n "+Color.END)
+# print(df['Claim_Amount'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Party Name\n "+Color.END)
-print(df['Party_Name'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Party Name\n "+Color.END)
+# print(df['Party_Name'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Party Surname\n "+Color.END)
-print(df['Party_Surname'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Party Surname\n "+Color.END)
+# print(df['Party_Surname'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Service Provider\n "+Color.END)
-print(df['Service_Provider'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Service Provider\n "+Color.END)
+# print(df['Service_Provider'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Holder Street Name\n "+Color.END)
-print(df['Policy_Holder_Street'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Holder Street Name\n "+Color.END)
+# print(df['Policy_Holder_Street'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Holder Province\n "+Color.END)
-print(df['Policy_Holder_Province'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Holder Province\n "+Color.END)
+# print(df['Policy_Holder_Province'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Holder City\n "+Color.END)
-print(df['Policy_Holder_City'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Holder City\n "+Color.END)
+# print(df['Policy_Holder_City'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Holder Area\n "+Color.END)
-print(df['Policy_Holder_Area'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Holder Area\n "+Color.END)
+# print(df['Policy_Holder_Area'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Policy Holder Postal Number\n "+Color.END)
-print(df['Policy_Holder_Postal'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Policy Holder Postal Number\n "+Color.END)
+# print(df['Policy_Holder_Postal'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Province where loss occurred\n "+Color.END)
-print(df['Province'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Province where loss occurred\n "+Color.END)
+# print(df['Province'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of City where loss occurred\n "+Color.END)
-print(df['City'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of City where loss occurred\n "+Color.END)
+# print(df['City'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Area where loss occurred\n "+Color.END)
-print(df['Area'].value_counts( dropna=False))
+# print(Color.BLUE+"\n\n\nFrequency of Area where loss occurred\n "+Color.END)
+# print(df['Area'].value_counts( dropna=False))
 
-print(Color.BLUE+"\n\n\nFrequency of Postal Code where loss occurred\n "+Color.END)
-print(df['Postal_Code'].value_counts( dropna=False))
-
+# print(Color.BLUE+"\n\n\nFrequency of Postal Code where loss occurred\n "+Color.END)
+# print(df['Postal_Code'].value_counts( dropna=False))
 
 
 
@@ -128,26 +138,26 @@ print(df['Postal_Code'].value_counts( dropna=False))
 # plt.show()
 
 ##### Numeric data info
-print(Color.DARKCYAN + "\n\n\nNumeric Data Information: " + Color.END)
-print(df.describe())
+# print(Color.DARKCYAN + "\n\n\nNumeric Data Information: " + Color.END)
+# print(df.describe())
 
 
-# df.plot() #plot(kind='scatter', x='SepalRatio', y='PetalRatio'))
+# df.plot()
 
 # plt.show()
 
 
-# df.mean().plot().show()
-
+# df.mean().plot()
+# plt.show()
 
 ##### Age distribution
 
 
 
 ##### Age for gender
-print(Color.CYAN + "Generating figure Gender VS Age"+ Color.END)
-df.groupby('Gender').Age.plot(kind='kde', legend=True, title='Gender vs Age', grid=True)
-plt.show()
+# print(Color.CYAN + "Generating figure Gender VS Age"+ Color.END)
+# df.groupby('Gender').Age.plot(kind='kde', legend=True, title='Gender vs Age', grid=True)
+# plt.show()
 
 
 
